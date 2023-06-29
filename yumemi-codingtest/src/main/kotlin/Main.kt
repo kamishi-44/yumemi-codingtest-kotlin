1467ed712496e5ef03e86d818102b02f16bc7f00
kamishi-44 <kamishioiri1016@gmail.com>
2023-06-30 00:00:55 +0900

    :sparkles: #3 引数のチェック処理を追加する。

--- yumemi-codingtest/src/main/kotlin/Main.kt
+++ yumemi-codingtest/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 /**
  * メイン関数
  *
@@ -5,6 +7,34 @@
  *                 [1]:ゲームのプレイログのファイル名)
  */
 fun main(args: Array<String>) {
+    // 引数の正当性チェック
     // ファイルの存在チェック
+    if (!validArgs(args)) {
+        // エラー処理
+    }
     // 引数ファイルの内容チェック
+}
+
+/**
+ * コマンドラインより渡ってきた引数が有効であるかを判定します。
+ * 以下全てを満たしている場合、有効と判定し true を返します。
+ * - 引数の配列の要素数が 2 である
+ * - 引数の各要素のファイルパスが存在し、ファイルである
+ *
+ * @param[args] コマンドライン引数
+ * @return コマンドライン引数の各要素が存在するファイルの場合 true
+ */
+private fun validArgs(args: Array<String>): Boolean {
+    // 引数の数が妥当か
+    if (args.size != 2) {
+        return false
+    }
+    // ファイルの存在チェック
+    for (filePath in args) {
+        val file: File = File(filePath)
+        if (!file.exists() || file.isDirectory) {
+            return false
+        }
+    }
+    return true
 }
