559fa826e2d35d8a7ca3675769362cb37be2ca3a
kamishi-44 <kamishioiri1016@gmail.com>
2023-07-26 23:29:25 +0900

    :sparkles: #3 読み込んだファイルを各データクラスで保持する

--- yumemi-codingtest/src/main/kotlin/Main.kt
+++ yumemi-codingtest/src/main/kotlin/Main.kt
@@ -20,8 +20,13 @@ fun main(args: Array<String>) {
     // ファイルの変換、内容チェック
     try {
         val importer = FileImporter(args)
-        val EntryPlayers: List<EntryPlayer> = importer.fileToEntryPlayer()
-        val PlayLogs: List<PlayLog> = importer.fileToPlayLog()
+        val entryPlayers: List<EntryPlayer> = importer.fileToEntryPlayer()
+        val playLogs: List<PlayLog> = importer.fileToPlayLog()
+        if (entryPlayers.isNotEmpty() && playLogs.isNotEmpty()){
+            println("取り込み成功")
+        } else {
+            println("取り込み失敗")
+        }
         return
     } catch (e: IOException) {
         println(e.message)
